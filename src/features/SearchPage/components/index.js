8adc3494478024845d4c88d4fd6781a470df7044
Ryan Collins <RyanCCollins@users.noreply.github.com>
2017-07-09 19:58:38 -0400

    Update index.js

--- src/features/SearchPage/components/index.js
+++ src/features/SearchPage/components/index.js
@@ -2,6 +2,7 @@ import SearchResults from './SearchResults';
 import SearchPresentation from './SearchPresentation';
 import SearchBar from './SearchBar';
 
+
 export {
   SearchResults,
   SearchPresentation,
